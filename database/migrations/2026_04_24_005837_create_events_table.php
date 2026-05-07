0458a7293d5ed8f6b3e8894eaebc5d2a09bf2ecb
allaudya06 <allaudya@students.amikom.ac.id>
2026-05-07 17:38:47 +0700

    Update:Tugas 5

--- database/migrations/2026_04_24_005837_create_events_table.php
+++ database/migrations/2026_04_24_005837_create_events_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            // Foreign Key relasi one-to-many ke categories
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->dateTime('date');
+            $table->string('location');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->string('poster_path')->nullable();
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,13 @@
 
 use App\Http\Controllers\Admin\CategoriesController;
 use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\EventsController;
+use App\Http\Controllers\Admin\EventController;
 use App\Http\Controllers\Admin\TransactionsController;
-use App\Http\Controllers\EventController;
+use App\Http\Controllers\EventController as PublicEventController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return '<h1>ini adalah halaman tentang aplikasi event hub</h1>';
-}); 
 
 Route::get('/', function () {
     return view('welcome');
@@ -33,18 +30,15 @@ Route::get('/bantuan', function () {
     return view('bantuan');
 });
 
-
-
 Route::get('/app', [HomeController::class,'index']);
 
-Route::get('/event-detail', [EventController::class, 'show']);
-Route::get('/checkout', [EventController::class, 'checkout']);
+Route::get('/event-detail', [PublicEventController::class, 'show']);
+Route::get('/checkout', [PublicEventController::class, 'checkout']);
 Route::get('/ticket', [TicketController::class, 'show']);
 
-Route::group(['prefix' => 'admin', 'as' =>'admin.' ],function(){
-
-    Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
-    Route::get('/events', [EventsController::class, 'index'])->name('events');
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('events', EventController::class);
     Route::get('/transactions', [TransactionsController::class, 'index'])->name('transactions');
     Route::get('/categories', [CategoriesController::class, 'index'])->name('categories');
 });
